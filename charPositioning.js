e2aa8e940aa269cb5729e0a7e759120e391f9194
raz346 <alhashimi346@gmail.com>
2017-04-05 22:12:25 +0000

    solved the problem and add js

---
+++ charPositioning.js
@@ -0,0 +1,22 @@
+
+// func to count letters and return obj
+function countLetters(sentence) {
+  var lettersCount = {} ;
+  //  go through the char of the string
+  for (var i = 0; i < sentence.length; i++) {
+    var currentChar = sentence[i];
+    if ( currentChar !== " "){
+    if ( currentChar in lettersCount) {
+      // push to the object
+      lettersCount[currentChar].push(i) ;
+
+    }
+  else { lettersCount[currentChar] = [i] ;}
+  };
+}
+  return lettersCount;
+}
+
+var output = countLetters("lighthouse in the house");
+console.log(output);
+
